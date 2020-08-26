1fd9352303dc1072d20420828a6516b051b8aa44
Phantonce <yes201045@gmail.com>
2020-08-26 13:51:34 +0100

    express and graphql enabled

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,38 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
+  const app = express();
+  app.get("/", (_, res) => {
+    res.send("Hello");
+  });
 
-    const orm = await MikroORM.init(microConfig);
-    const post = orm.em.create(Post, { title: 'my first post' })
-    await orm.em.persistAndFlush(post)
-}
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
 
-main().catch((err) => {
-    console.error(err)
-})
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("Server started on port 4000");
+  });
+  //   const post = orm.em.create(Post, { title: "my first post" });
+  //     await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts)
+};
+
+main().catch(err => {
+  console.error(err);
+});
